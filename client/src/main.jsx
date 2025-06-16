754c4323c629ce9c59ec0ed68b9664432dc48345
Aayush <aayush@Aayushs-MacBook-Air.local>
2025-06-16 20:29:28 +0530

    16 June 2025

---
+++ client/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+import {BrowserRouter} from 'react-router-dom'
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+    // <App />
+  // </StrictMode>,
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
+)
