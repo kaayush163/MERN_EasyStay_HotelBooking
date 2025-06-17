e1076d5fd73d3184108b755333682df4d8870b52
Aayush <aayush@Aayushs-MacBook-Air.local>
2025-06-17 09:22:29 +0530

    Testimonial component build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
 import FeatureDestination from "../components/featureDestination";
+import ExclusiveOffers from "../components/ExclusiveOffers";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
     <>
       <Hero />
-
       <FeatureDestination />
+      <ExclusiveOffers />
+      <Testimonial />
     </>
   );
 };
