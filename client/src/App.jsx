a95f9a55fd76ddd0372e370d1f923d5b2ceffdd9
Aayush <aayush@Aayushs-MacBook-Air.local>
2025-06-19 09:10:26 +0530

    hotel registeration page made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,14 @@ import AllRooms from "./pages/AllRooms";
 import Footer from "./components/Footer";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
+import HotelReg from "./components/HotelReg";
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner"); //this will hide the nav bar for owner path /owner
 
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
