daa8003241e9596feaf03ba34dfe5ee3a3ae8dc4
Aayush <kaayush163@gmail.com>
2025-06-22 13:35:45 +0530

    created booking Controller,routes,models for creating booking checking availabilty

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import clerkWebHooks from "./controllers/clerkWebHooks.js";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
 import roomRouter from "./routes/roomRoutes.js";
-// import bookingRouter from "./routes/bookingRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 // import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import connectCloudinary from "./configs/cloudinary.js";
 // import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
@@ -30,7 +30,7 @@ app.get("/", (req, res) => res.send("API is working fine with 200OK"));
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/rooms", roomRouter);
-// app.use("/api/bookings", bookingRouter);
+app.use("/api/bookings", bookingRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
