d15e70d73454777a1cece02794614d449845bae6
Aayush <kaayush163@gmail.com>
2025-06-22 08:11:34 +0530

    Get User and Searched recent city APIs created

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebHooks from "./controllers/clerkWebHooks.js";
-// import userRouter from "./routes/userRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 // import hotelRouter from "./routes/hotelRoutes.js";
 // import roomRouter from "./routes/roomRoutes.js";
 // import bookingRouter from "./routes/bookingRoutes.js";
@@ -26,8 +26,8 @@ app.use(clerkMiddleware());
 
 app.use("/api/clerk", clerkWebHooks);
 
-app.get("/", (req, res) => res.send("API is working fine"));
-// app.use("/api/user", userRouter);
+app.get("/", (req, res) => res.send("API is working fine with 200OK"));
+app.use("/api/user", userRouter);
 // app.use("/api/hotels", hotelRouter);
 // app.use("/api/rooms", roomRouter);
 // app.use("/api/bookings", bookingRouter);
