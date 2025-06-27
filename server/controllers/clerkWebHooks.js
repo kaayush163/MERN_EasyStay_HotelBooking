424df6a927763dd2dc5c7ee9198f05f21f91bc33
Aayush <kaayush163@gmail.com>
2025-06-28 00:56:13 +0530

    some amendments

--- server/controllers/clerkWebHooks.js
+++ server/controllers/clerkWebHooks.js
@@ -5,31 +5,13 @@ import User from "../models/User.js";
 // POST /api/clerk
 const clerkWebhooks = async (req, res) => {
   try {
-    // const evt = await verifyWebhook(req);
-
-    // // Do something with payload
-    // // For this guide, log payload to console
-    // const { id } = evt.data;
-    // const eventType = evt.type;
-    // console.log(
-    //   `Received webhook with ID ${id} and event type of ${eventType}`
-    // );
-    // console.log("Webhook payload:", evt.data);
-
     // Create a Svix instance with clerk webhook secret.
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    const timestamp = Math.floor(Date.now() / 1000);
     // Getting Headers
     const headers = {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
-      // "svix-id": req.headers["id"],
-      // "svix-timestamp": req.headers["timestamp"],
-      // "svix-signature": req.headers["signature"],
-      // "svix-id": "msg_2yrUlCpRDNpetRYyyYH3mpMxXBf",
-      // "svix-timestamp": "1750592334",
-      // "svix-signature": "v1,OUrK/4qQF9B9NrwLjTqtwu+nP6X3e5/0BZ5PqGWRYzg=",
     };
 
     // Verifying Headers
@@ -68,10 +50,7 @@ const clerkWebhooks = async (req, res) => {
 
     res.json({ success: true, message: "Webhook Recieved by Svix" });
   } catch (error) {
-    console.log(res);
-
     console.log(error.message);
-
     res.json({ success: false, message: error.message });
   }
 };
