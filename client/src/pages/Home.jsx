038ef6cefdc0fddc153613b53882ae6283ff1df9
Aayush <kaayush163@gmail.com>
2025-06-28 17:10:06 +0530

    alphbetic error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
-import FeatureDestination from "../components/featureDestination";
+import FeatureDestination from "../components/FeatureDestination";
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import Testimonial from "../components/Testimonial";
 import NewsLetter from "../components/NewsLetter";
